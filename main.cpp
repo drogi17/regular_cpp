7d2d1dde54e753ccd2b0dd37d41e14fd75eb3c1c
drogi17 <38162697+drogi17@users.noreply.github.com>
2020-04-10 11:57:01 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ using std::endl;
 using std::cout;
 using std::cin;
 
+void regular_show(regular &reg, int num){
+	cout << "regular: " << num << endl;
+	reg.view();
+}
 
 void print_regular(regular &reg){
     reg.view();
@@ -13,34 +17,35 @@ void print_regular(regular &reg){
 
 int main() {
     regular reg, reg1;
+    cout << "Add: " << endl;
     reg.add(1).add(4).add(10).add(228).add_to_start(121).add_to_start(121).add_to_start(414123);
     for (int i = 0; i < 10; i++){
         reg.add(9-i);
     }
-    reg.view();
+    regular_show(reg, 1);
     cout << endl << endl;
+    cout << "Sort: " << endl;
+    regular_show(reg, 1);
     reg.sort();
-    reg.view();
-    reg1.view();
-    reg1.view();
-    cout << reg1[0];
-    reg1.view();
-    reg1.view();
+    regular_show(reg, 1);
+    // regular_show(reg1, 2);
     cout << endl << endl;
-    reg.view();
-    reg1.view();
-    cout << endl << endl;
+    cout << "Operator (=): " << endl;
     reg1 = reg;
-    reg.view();
-    reg1.view();
+    regular_show(reg, 1);
+    regular_show(reg1, 2);
     cout << endl << endl;
-    print_regular(reg1);
-    reg1.view();
+    cout << "clear: " << endl;
+    regular_show(reg1, 2);
     reg1.clear();
+    regular_show(reg1, 2);
     reg1.add(1).add(4).add(10).add(228).view();
+    cout << endl << endl;
     cout << "len1: " << reg.len() <<endl;
     cout << "len2: " << reg1.len() <<endl;
     cout << "get: " << reg.get(3) <<endl;
+    cout << endl << endl;
+    cout << "remove: " << endl;
     reg.remove(3);
     cout << "get: " << reg[3] <<endl;
     reg[3] = 10;
